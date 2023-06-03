6f51dfce57446ac215d5ce20fde96e7f2a1ab9f5
ciel0217 <karimu0217@icloud.com>
2023-06-03 20:28:11 +0900

    a

--- DirectX12/Framework/MemoryAllocator/TLSFAllocator.cpp
+++ DirectX12/Framework/MemoryAllocator/TLSFAllocator.cpp
@@ -6,9 +6,15 @@ void TLSFAllocator::ShutDown()
 
 void * TLSFAllocator::DivideMemory(unsigned int block_num)
 {
+	//block_num‚ÌÅãˆÊ1Bit‚ğæ“¾
 	unsigned int divide_bit = HigherBit(block_num);
+	//ÅãˆÊ1Bit‚©‚ç¶‘¤‚ğ1‚Å–„‚ß‚é
 	unsigned int mask = ~(FillRightBit(divide_bit) >> 1);
+	//FreeFlags‚Æmask‚ÅA‹ó‚¢‚Ä‚¢‚é—Ìˆæ‚ğæ“¾
 	unsigned int free_bit = (m_FreeFlags & mask);
+
+
+
 
 	return nullptr;
 }
