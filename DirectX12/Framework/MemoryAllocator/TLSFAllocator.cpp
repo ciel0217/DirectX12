2884d6a767830b223e7cfc69010f332cca60f838
ciel0217 <karimu0217@icloud.com>
2023-06-05 12:43:46 +0900

    a

--- DirectX12/Framework/MemoryAllocator/TLSFAllocator.cpp
+++ DirectX12/Framework/MemoryAllocator/TLSFAllocator.cpp
@@ -4,14 +4,27 @@ void TLSFAllocator::ShutDown()
 {
 }
 
-void * TLSFAllocator::DivideMemory(unsigned int block_num)
+void * TLSFAllocator::DivideMemory(unsigned int block_size)
 {
 	//block_num‚ÌÅãˆÊ1Bit‚ğæ“¾
-	unsigned int divide_bit = HigherBit(block_num);
+	unsigned int divide_bit = HigherBit(block_size);
+
+	unsigned int mask = (1 << divide_bit) - 1;
+
+	const unsigned int rs = mask - 5;
+
+	unsigned int index = (divide_bit & mask) >> rs;
+
+	if (m_FreeList[divide_bit * 32 + index]->m_Size == block_size)
+	{
+
+	}
+	
+
 	//ÅãˆÊ1Bit‚©‚ç¶‘¤‚ğ1‚Å–„‚ß‚é
-	unsigned int mask = ~(FillRightBit(divide_bit) >> 1);
+	unsigned int my_bit = ~(FillRightBit(divide_bit) >> 1);
 	//FreeFlags‚Æmask‚ÅA‹ó‚¢‚Ä‚¢‚é—Ìˆæ‚ğæ“¾
-	unsigned int free_bit = (m_FreeFlags & mask);
+	unsigned int free_bit = (m_FreeFlags & my_bit);
 
 
 
